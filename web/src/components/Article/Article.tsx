b39df15ef2910169de67049b5f7c1627931a9dd2
KPInc425 <kpinc425@gmail.com>
2023-03-24 13:59:48 -0700

    Add comments functionality with db schema, testing, and services

--- web/src/components/Article/Article.tsx
+++ web/src/components/Article/Article.tsx
@@ -1,7 +1,15 @@
 import { Link, routes } from '@redwoodjs/router'
 
+import CommentsCell from 'src/components/CommentsCell'
+
 const truncate = (text, length) => {
   return text.substring(0, length) + '...'
+}
+
+const formattedDate = (datetime) => {
+  const parsedDate = new Date(datetime)
+  const month = parsedDate.toLocaleString('default', { month: 'long' })
+  return `${month} ${parsedDate.getDate()}, ${parsedDate.getFullYear()}`
 }
 
 const Article = ({ article, summary = false }) => {
@@ -9,14 +17,19 @@ const Article = ({ article, summary = false }) => {
   return (
     <article>
       <header>
-        <h2 className="text-xl text-blue-700 font-semibold">
+        <h2 className="text-xl font-semibold text-blue-700">
           <Link to={routes.article({ id: article.id })}>{article.title}</Link>
         </h2>
       </header>
-      <div className="mt-2 text-gray-900 font-light">
+      <div className="mt-2 font-light text-gray-900">
         {summary ? truncate(article.body, 100) : article.body}
       </div>
-      <div>Posted at: {article.createdAt}</div>
+      <div>Posted on: {formattedDate(article.createdAt)}</div>
+      {!summary && (
+        <div className="mt-8">
+          <CommentsCell />
+        </div>
+      )}
     </article>
   )
 }
